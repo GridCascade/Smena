4e82937d6abff05b2339c821e892f32c28345ce6
Dmitrii Ivanov <dmitrii@getqardio.com>
2018-04-28 22:12:07 +0200

    Smena: - AbstractPlayer protocol for coverring different implementations (high level, low level) SmenaAppL - manual instantiating of VCs - VCFactory for instantiating VCs - splitting PlayerVC to VC and it's model

---
+++ example/SmenaApp/Sources/VCFactory.swift
@@ -0,0 +1,37 @@
+//
+//  VCFactory.swift
+//  SmenaApp
+//
+//  Created by Dmitrii on 28/04/2018.
+//  Copyright © 2018 Grid Cascade. All rights reserved.
+//
+
+import UIKit
+
+
+class VCFactory: NSObject {
+
+    func mainStoryboard() -> UIStoryboard {
+        return UIStoryboard(name: "Main", bundle: nil)
+    }
+
+    func firstVC() -> ViewController {
+        return mainStoryboard().instantiateInitialViewController() as! ViewController
+    }
+
+    func playerVC() -> PlayerVC {
+        return mainStoryboard().instantiateViewController(withIdentifier: String(describing: PlayerVC.self)) as! PlayerVC
+    }
+
+    func recorderVC() -> RecorderVC {
+        return mainStoryboard().instantiateViewController(withIdentifier: String(describing: RecorderVC.self)) as! RecorderVC
+    }
+}
+
+
+extension UIViewController {
+
+    func wrapped() -> UINavigationController {
+        return UINavigationController.init(rootViewController: self)
+    }
+}
